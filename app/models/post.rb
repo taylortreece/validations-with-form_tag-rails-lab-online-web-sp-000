a4de51792d782e18741a902a2094ee1a0b463204
taylortreece <tayloratreece@gmail.com>
2021-04-21 04:44:58 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
+    validates :category, acceptance: { accept: [ "Fiction", "Non-Fiction" ]}
 end
